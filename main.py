dd4f6bbcfe500e785ffcad087d514846fda06015
mgmike1011 <60261042+mgmike1011@users.noreply.github.com>
2022-01-19 19:11:50 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+# Wprowadzenie do sztucznej inteligencji - laboratorium - projekt zaliczeniowy 
+import os
+import random
+import numpy as np
+import cv2
+from sklearn.ensemble import RandomForestClassifier
+import pandas
